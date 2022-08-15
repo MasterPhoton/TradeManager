6c1c51441ad669e101ad582cb88dda26d0f46454
MasterPhoton <shankhadeep211p@gmail.com>
2022-08-16 00:28:36 +0530

    Item location update asset

--- src/main/java/minecraft/sbsdgrr/registry/ModItems.java
+++ src/main/java/minecraft/sbsdgrr/registry/ModItems.java
@@ -3,17 +3,21 @@ package minecraft.sbsdgrr.registry;
 import minecraft.sbsdgrr.TradeManager;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 
-public class ModItems {
+public class ModItems{
 
     /*Creating the item objects*/
     public static final Item VENDING_MACHINE= new Item(new Item.Settings().group(ItemGroup.DECORATIONS).maxCount(1));
+
 
     /*Register The Items*/
     public static void registerItems(){
         Registry.register(Registry.ITEM,new Identifier(TradeManager.MOD_ID, "vendingmachine"), VENDING_MACHINE);
     }
+
 }
