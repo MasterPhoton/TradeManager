c07db866ae904ff3773187a91258a02f91221146
MasterPhoton <shankhadeep211p@gmail.com>
2022-08-15 21:54:38 +0530

    mod menu update

---
+++ src/main/java/minecraft/sbsdgrr/TradeManager.java
@@ -0,0 +1,13 @@
+package minecraft.sbsdgrr;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TradeManager implements ModInitializer {
+
+    public static final String MOD_ID= "trademanager";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
