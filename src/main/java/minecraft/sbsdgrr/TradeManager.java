61b2b3de7e59bde506c238284edd3b3b0798ab0d
subha <subha@DESKTOP-VR5VDP5>
2022-08-20 20:14:33 +0530

    Optimized

--- src/main/java/minecraft/sbsdgrr/TradeManager.java
+++ src/main/java/minecraft/sbsdgrr/TradeManager.java
@@ -1,39 +1,27 @@
 package minecraft.sbsdgrr;
 
-import com.mojang.datafixers.DataFixerBuilder;
-import com.mojang.datafixers.schemas.Schema;
-import minecraft.sbsdgrr.registry.ModBlocks;
-import minecraft.sbsdgrr.registry.ModItems;
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.minecraft.util.Util;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import minecraft.sbsdgrr.registry.ModBlocks;
+import minecraft.sbsdgrr.registry.ModItems;
+import net.fabricmc.api.ModInitializer;
+
 public class TradeManager implements ModInitializer {
-    private static final Logger log = LogManager.getLogger(TradeManager.class);
+	private static final Logger log = LogManager.getLogger(TradeManager.class);
 
-    public static final String MOD_ID = "trademanager";
+	public static final String MOD_ID = "trademanager";
 
+	@Override
+	public void onInitialize() {
+		try {
+			ModItems.registerItems();
+			ModBlocks.registerBlocks();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-//    public void onInitializeServer() {
-//        log.info("TradeManager Server Initialized ");
-//    }
-
-    @Override
-    public void onInitialize() {
-        try {
-            ModItems.registerItems();
-            ModBlocks.registerBlocks();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-
-
-        log.info("TradeManager ModInit");
-    }
-
-
+		log.info("TradeManager ModInit");
+	}
 
 }
--- src/main/java/minecraft/sbsdgrr/registry/ModKeys.java
+++ src/main/java/minecraft/sbsdgrr/registry/ModKeys.java
@@ -1,126 +1,117 @@
 package minecraft.sbsdgrr.registry;
 
-import minecraft.sbsdgrr.keybinds.ActionKey;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.fabricmc.fabric.api.entity.event.v1.FabricElytraItem;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.fabricmc.fabric.api.item.v1.FabricItem;
-import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.client.network.ClientPlayerInteractionManager;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.inventory.Inventory;
-import net.minecraft.inventory.SimpleInventory;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.scoreboard.Scoreboard;
-import net.minecraft.screen.slot.SlotActionType;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Style;
-import net.minecraft.text.Text;
-import net.minecraft.util.profiler.Sampler;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.HashSet;
-import java.util.function.Predicate;
+import minecraft.sbsdgrr.keybinds.ActionKey;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.SimpleInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.screen.slot.SlotActionType;
+import net.minecraft.text.Text;
 
 public class ModKeys {
-    private static final Logger log = LogManager.getLogger(ModKeys.class);
+	private static final Logger log = LogManager.getLogger(ModKeys.class);
 
-    public static final ActionKey ACTION_KEY = new ActionKey();
-    public static HashSet<ItemStack> items = new HashSet<>();
+	public static final ActionKey ACTION_KEY = new ActionKey();
+	protected static Set<ItemStack> items = new HashSet<>();
 
-    public static void registerKeyBinds() {
-        KeyBindingHelper.registerKeyBinding(ACTION_KEY);
-    }
+	private ModKeys() {
+	}
 
-    public static void keyPressEvents() throws Exception {
+	public static void registerKeyBinds() {
+		KeyBindingHelper.registerKeyBinding(ACTION_KEY);
+	}
 
-        /*Creating even listener*/
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (ModKeys.ACTION_KEY.wasPressed()) {
-                /*Player Entity*/
-                PlayerEntity player = client.player;
-                /*creating necessary variables*/
-                int chest_slot = 38;
-                int swap_slot = 100;
-                ItemStack chestItem = null;
-                ItemStack swapItem = null;
-                int chestFlag = 0;
-                int flight = 0;
-                /*check if swapping possible or not*/
-                if (!player.getInventory().getStack(chest_slot).isEmpty()) {
-                    chestItem = player.getInventory().getStack(chest_slot);
-                    chestFlag = 1;
-                    log.info("Chest item on " + chestItem.toString());
-                } else {
-                    log.info("Must equip chest item before swapping");
-                }
-                /*checking for swap item in player inventory*/
-                for (int i = 0; i <= 35; i++) {
-                    if (!player.getInventory().getStack(i).isEmpty()) {
-                        ItemStack item = player.getInventory().getStack(i);
-                        if (chestFlag == 1 && chestItem.toString().contains("chestplate")) {
-                            if (item.toString().contains("elytra")) {
-                                swap_slot = i;
-                                swapItem = item;
-                                flight = 1;
-                                log.info("arm=> " + chestItem.toString() + "|| found elytra");
-                                chestFlag = 2;
-                                break;
-                            }
-                        }
-                        if (chestFlag == 1 && chestItem.toString().contains("elytra")) {
-                            if (item.toString().contains("chestplate")) {
-                                swap_slot = i;
-                                swapItem = item;
-                                log.info("fly=> " + chestItem.toString() + "|| found chestplate");
-                                chestFlag = 2;
-                                break;
-                            }
-                        }
-                    }
-                }
-                /*check if all requirements are passed*/
-                if (chestFlag < 2) {
-                    player.sendMessage(Text.literal("Armor or Elytra not found in " + player.getDisplayName().getString() + " Inventory"), true);
-                } else {
-                    /*temp item var*/
-                    ItemStack tempItem = swapItem;
-                    PlayerInventory inv = player.getInventory();
+	public static void keyPressEvents() {
+		/* Creating even listener */
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			while (ModKeys.ACTION_KEY.wasPressed()) {
+				/* Player Entity */
+				PlayerEntity player = client.player;
+				/* creating necessary variables */
+				int chestSlot = 38;
+				int swapSlot = 100;
+				ItemStack chestItem = null;
+				int chestFlag = 0;
+				int flight = 0;
+				/* check if swapping possible or not */
+				if (!player.getInventory().getStack(chestSlot).isEmpty()) {
+					chestItem = player.getInventory().getStack(chestSlot);
+					chestFlag = 1;
+					log.info(("Chest item on " + chestItem.toString()));
+				} else {
+					log.info("Must equip chest item before swapping");
+				}
+				/* checking for swap item in player inventory */
+				boolean flag = false;
+				for (int i = 0; i <= 35; i++) {
+					if (!player.getInventory().getStack(i).isEmpty()) {
+						ItemStack item = player.getInventory().getStack(i);
+						if (chestFlag == 1 && chestItem.toString().contains("chestplate")
+								&& item.toString().contains("elytra")) {
+							swapSlot = i;
+							flight = 1;
+							log.info(("arm=> " + chestItem.toString() + "|| found elytra"));
+							chestFlag = 2;
+							flag = true;
 
-                    /*exp*/
-                    ClientPlayerInteractionManager interactionManager = client.interactionManager;
-                    SimpleInventory simpleInventory=new SimpleInventory();
+						}
+						if (!flag && chestFlag == 1 && chestItem.toString().contains("elytra")
+								&& item.toString().contains("chestplate")) {
+							swapSlot = i;
+							log.info(("fly=> " + chestItem.toString() + "|| found chestplate"));
+							chestFlag = 2;
+							flag = true;
+						}
+						if (flag) {
+							break;
+						}
+					}
+				}
+				/* check if all requirements are passed */
+				if (chestFlag < 2) {
+					player.sendMessage(Text.literal(
+							"Armor or Elytra not found in " + player.getDisplayName().getString() + " Inventory"),
+							true);
+				} else {
+					player.getInventory();
 
+					/* exp */
+					ClientPlayerInteractionManager interactionManager = client.interactionManager;
+					new SimpleInventory();
 
-                    /*interaction button starts from Hotbar[1-9] then it takes slot values. quite complicated*/
-                    swap_slot=(swap_slot<9)?(swap_slot+36):swap_slot;
+					/*
+					 * interaction button starts from Hotbar[1-9] then it takes slot values. quite
+					 * complicated
+					 */
+					swapSlot = (swapSlot < 9) ? (swapSlot + 36) : swapSlot;
 
+					/* swap elytra with chest plate and display msg */
+					if (flight == 0) {
+						if (player.isFallFlying()) {
+							player.sendMessage(Text.literal("Elytra is active"), true);
+						} else {
+							interactionManager.clickSlot(player.playerScreenHandler.syncId, swapSlot, chestSlot,
+									SlotActionType.SWAP, player);
+							player.sendMessage(Text.literal("Flight Off"), true);
+						}
+					} else if (flight == 1) {
+						interactionManager.clickSlot(player.playerScreenHandler.syncId, swapSlot, chestSlot,
+								SlotActionType.SWAP, player);
+						player.sendMessage(Text.literal("Flight On"), true);
+					}
+				}
 
-                    /*swap elytra with chestplate and display msg*/
-                    if (flight == 0) {
-                        if (player.isFallFlying()) {
-                            player.sendMessage(Text.literal("Elytra is active"), true);
-                        } else {
-                            interactionManager.clickSlot(player.playerScreenHandler.syncId, swap_slot,chest_slot, SlotActionType.SWAP,player);
-                            player.sendMessage(Text.literal("Flight Off"), true);
-                        }
-                    } else if (flight == 1) {
-                        interactionManager.clickSlot(player.playerScreenHandler.syncId, swap_slot,chest_slot, SlotActionType.SWAP,player);
-                        player.sendMessage(Text.literal("Flight On"), true);
-                    }
-                }
+			}
+		});
 
-
-            }
-        });
-
-
-    }
-
+	}
 
 }
